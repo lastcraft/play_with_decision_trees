0e5f1dcee205fc55b0019420bb97b322c3434600
marcus <marcus@conversocial.com>
2019-06-24 11:12:09 +0100

    Predicate module

--- decision_tree.py
+++ decision_tree.py
@@ -1,13 +1,8 @@
-from math import log, floor
+from predicate import (
+    NoData, OutOfDomain,
+    FiniteDomain, MissingMiddle, IsNone, IsSame, IsEqualOrGreaterThan)
+from math import log
 log2 = lambda x: log(x) / log(2)
-
-
-class NoData(Exception):
-    pass
-
-
-class OutOfDomain(Exception):
-    pass
 
 
 class Tree(object):
@@ -43,73 +38,6 @@ class Leaf(object):
 
     def __str__(self, indent=''):
         return "\n" + indent + '\\---' + str(self.conclusion)
-
-
-class FiniteDomain(object):
-    def __init__(self, values):
-        self.values = values
-
-    def is_in(self, value):
-        return value in self.values
-
-
-class MissingMiddle(object):
-    def __init__(self, lower, upper):
-        self.lower = lower
-        self.upper = upper
-
-    def is_in(self, value):
-        return value <= self.lower or value >= self.upper
-
-
-class IsSame(object):
-    def __init__(self, key, value, domain=None):
-        self.key = key
-        self.value = value
-        self.domain = domain
-
-    def __call__(self, data):
-        if self.key not in data:
-            raise NoData()
-        if not self.domain.is_in(data[self.key]):
-            raise OutOfDomain()
-        return data[self.key] == self.value
-
-    def __str__(self):
-        return "%s is same as %s" % (self.key, self.value)
-
-
-class IsEqualOrGreaterThan(object):
-    def __init__(self, key, value, domain=None):
-        self.key = key
-        self.value = value
-        self.domain = domain
-
-    def __call__(self, data):
-        if self.key not in data:
-            raise NoData()
-        if not self.domain.is_in(data[self.key]):
-            raise OutOfDomain()
-        return data[self.key] >= self.value
-
-    def __str__(self):
-        return "%s is greater than %s" % (self.key, self.value)
-
-
-class IsNone(object):
-    def __init__(self, key, domain=None):
-        self.key = key
-        self.domain = domain
-
-    def __call__(self, data):
-        if self.key not in data:
-            return True
-        if not self.domain.is_in(data[self.key]):
-            raise OutOfDomain()
-        return data[self.key] is None
-
-    def __str__(self):
-        return "%s is missing" % self.key
 
 
 def build(examples):
---
+++ predicate.py
@@ -0,0 +1,74 @@
+
+class NoData(Exception):
+    pass
+
+
+class OutOfDomain(Exception):
+    pass
+
+
+class FiniteDomain(object):
+    def __init__(self, values):
+        self.values = values
+
+    def is_in(self, value):
+        return value in self.values
+
+
+class MissingMiddle(object):
+    def __init__(self, lower, upper):
+        self.lower = lower
+        self.upper = upper
+
+    def is_in(self, value):
+        return value <= self.lower or value >= self.upper
+
+
+class IsSame(object):
+    def __init__(self, key, value, domain=None):
+        self.key = key
+        self.value = value
+        self.domain = domain
+
+    def __call__(self, data):
+        if self.key not in data:
+            raise NoData()
+        if not self.domain.is_in(data[self.key]):
+            raise OutOfDomain()
+        return data[self.key] == self.value
+
+    def __str__(self):
+        return "%s is same as %s" % (self.key, self.value)
+
+
+class IsEqualOrGreaterThan(object):
+    def __init__(self, key, value, domain=None):
+        self.key = key
+        self.value = value
+        self.domain = domain
+
+    def __call__(self, data):
+        if self.key not in data:
+            raise NoData()
+        if not self.domain.is_in(data[self.key]):
+            raise OutOfDomain()
+        return data[self.key] >= self.value
+
+    def __str__(self):
+        return "%s is greater than %s" % (self.key, self.value)
+
+
+class IsNone(object):
+    def __init__(self, key, domain=None):
+        self.key = key
+        self.domain = domain
+
+    def __call__(self, data):
+        if self.key not in data:
+            return True
+        if not self.domain.is_in(data[self.key]):
+            raise OutOfDomain()
+        return data[self.key] is None
+
+    def __str__(self):
+        return "%s is missing" % self.key
