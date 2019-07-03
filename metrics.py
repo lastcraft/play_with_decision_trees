0873d2118c5bb0978746af0066210050b7560a99
Marcus Baker <marcus@conversocial.com>
2019-07-03 10:59:56 +0100

    Adding variance

--- metrics.py
+++ metrics.py
@@ -3,9 +3,28 @@ log2 = lambda x: log(x) / log(2)
 
 
 def calculate_gain(both, yesses, nos):
-    return measure_entropy(probability_distribution(both)) \
-        - len(yesses)/len(both) * measure_entropy(probability_distribution(yesses)) \
-        - len(nos)/len(both) * measure_entropy(probability_distribution(nos))
+    return measure_entropy(both) \
+        - len(yesses)/len(both) * measure_entropy(yesses) \
+        - len(nos)/len(both) * measure_entropy(nos)
+
+
+def variance(examples):
+    mean = sum([example['conculsion'] for example in examples])
+    return sum([(example['conclusion'] - mean) ** 2 for example in examples])
+
+
+def measure_entropy(examples):
+    probabilities = probability_distribution(examples)
+    entropy = 0.0
+    for key, probability in probabilities.iteritems():
+        entropy -= probability * log2(probability)
+    return entropy
+
+
+def probability_distribution(examples):
+    counts = histogram(examples)
+    total = sum(counts.values())
+    return {key: float(value)/total for key, value in counts.iteritems()}
 
 
 def histogram(examples):
@@ -16,16 +35,3 @@ def histogram(examples):
         else:
             counts[example['conclusion']] += 1
     return counts
-
-
-def probability_distribution(examples):
-    counts = histogram(examples)
-    total = sum(counts.values())
-    return {key: float(value)/total for key, value in counts.iteritems()}
-
-
-def measure_entropy(probabilities):
-    entropy = 0.0
-    for key, probability in probabilities.iteritems():
-        entropy -= probability * log2(probability)
-    return entropy
