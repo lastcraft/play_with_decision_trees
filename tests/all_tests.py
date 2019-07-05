1c3c4b5ac4dda8632d2b5eede1036f5d16a7f74c
Marcus Baker <marcus@conversocial.com>
2019-07-05 19:39:14 +0100

    Refactoring tests

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,4 +1,4 @@
-from decision_tree_builder import build, prune
+from decision_tree_builder import build
 from tests.logical_deduction_tests import test_logical_deductions
 from tests.many_choices_tests import test_option_with_many_choices
 from tests.metrics_tests import test_statistics, test_gain
@@ -29,35 +29,6 @@ def all_tests():
     test_coping_with_unseen_options()
     test_choosing_from_numerical_data()
     test_numerical_evaluations()
-
-
-def test_can_prune_tree_with_missing_example_data():
-    examples = [{'data': {'weather': 'sunny'}, 'conclusion': 'happy'},
-                {'data': {}, 'conclusion': 'cheerful'},
-                {'data': {'weather': 'rainy'}, 'conclusion': 'sad'}]
-    tree = build(examples)
-    tree = prune(examples, tree, threshold=0.1)
-    assert(tree.decide({'weather': 'sunny'}) == {'happy': 1.0})
-    # assert(tree.decide({}) == {'cheerful': 1.0})
-    assert(tree.decide({'weather': 'rainy'}) == {'sad': 1.0})
-
-
-def test_prune_can_remove_missing_data_outliers():
-    examples = [{'data': {'weather': 'sunny'}, 'conclusion': 'happy'}] * 99
-    examples += [{'data': {}, 'conclusion': 'sad'}]
-    tree = build(examples)
-    tree = prune(examples, tree, threshold=0.1)
-    assert(tree.decide({'weather': 'sunny'}) == {'happy': 1.0})
-    assert(tree.decide({}) == {'happy': 1.0})
-
-
-def test_prune_can_remove_outliers_leaving_missing_example_data():
-    examples = [{'data': {}, 'conclusion': 'happy'}] * 99
-    examples += [{'data': {'weather': 'rainy'}, 'conclusion': 'sad'}]
-    tree = build(examples)
-    tree = prune(examples, tree, threshold=0.1)
-    assert(tree.decide({}) == {'happy': 1.0})
-    assert(tree.decide({'weather': 'rainy'}) == {'happy': 1.0})
 
 
 def test_unseen_option_treated_as_missing_data():
--- tests/pruning_tests.py
+++ tests/pruning_tests.py
@@ -1,5 +1,4 @@
 from decision_tree_builder import build, prune
-from tests.all_tests import test_can_prune_tree_with_missing_example_data
 
 
 def test_pruning():
@@ -34,3 +33,32 @@ def test_no_pruning_unless_gain_is_less_than_threshold():
     tree = prune(examples, tree, threshold=0.01)
     assert(tree.decide({'weather': 'sunny'}) == {'happy': 1.0})
     assert(tree.decide({'weather': 'rainy'}) == {'sad': 1.0})
+
+
+def test_can_prune_tree_with_missing_example_data():
+    examples = [{'data': {'weather': 'sunny'}, 'conclusion': 'happy'},
+                {'data': {}, 'conclusion': 'cheerful'},
+                {'data': {'weather': 'rainy'}, 'conclusion': 'sad'}]
+    tree = build(examples)
+    tree = prune(examples, tree, threshold=0.1)
+    assert(tree.decide({'weather': 'sunny'}) == {'happy': 1.0})
+    # assert(tree.decide({}) == {'cheerful': 1.0})
+    assert(tree.decide({'weather': 'rainy'}) == {'sad': 1.0})
+
+
+def test_prune_can_remove_missing_data_outliers():
+    examples = [{'data': {'weather': 'sunny'}, 'conclusion': 'happy'}] * 99
+    examples += [{'data': {}, 'conclusion': 'sad'}]
+    tree = build(examples)
+    tree = prune(examples, tree, threshold=0.1)
+    assert(tree.decide({'weather': 'sunny'}) == {'happy': 1.0})
+    assert(tree.decide({}) == {'happy': 1.0})
+
+
+def test_prune_can_remove_outliers_leaving_missing_example_data():
+    examples = [{'data': {}, 'conclusion': 'happy'}] * 99
+    examples += [{'data': {'weather': 'rainy'}, 'conclusion': 'sad'}]
+    tree = build(examples)
+    tree = prune(examples, tree, threshold=0.1)
+    assert(tree.decide({}) == {'happy': 1.0})
+    assert(tree.decide({'weather': 'rainy'}) == {'happy': 1.0})
